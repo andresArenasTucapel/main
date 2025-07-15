5b99e9111ee6eed5a6381bd3a0e40ac1dd8bcb77
andresArenasTucapel <andresc.arenas@gmail.com>
2025-07-15 16:42:49 -0400

    Create HolaMundo.py

---
+++ HolaMundo.py
@@ -0,0 +1,10 @@
+# Clona el repo
+git clone https://github.com/TU_USUARIO/streamlit-licitaciones.git
+cd streamlit-licitaciones
+
+# Copia tus archivos: app.py, requirements.txt, .streamlit/secrets.toml (este último NO lo subas)
+
+# Agrega y sube
+git add .
+git commit -m "primer commit"
+git push origin main
